27de88f1e01489afc8e2d4d7276abd2bd4fd837a
PanYuchong <panyuchong@gmail.com>
2016-08-03 14:10:44 +0800

    Refine Exercise 1.5

--- ex-1.5.scm
+++ ex-1.5.scm
@@ -1,10 +1,5 @@
 ;; Applicative-order evaluation will first evaluate the subexpression (p). However, to evaluate (p), the interpretor will again evaluate
-;; (p), according to the definition of the precedure (p). Hence, the evaluation will get stuck in an infinite recursion.
+;; (p), according to the definition of the precedure (p), and so on. Hence, the evaluation will get stuck in an infinite recursion.
 ;;
-;; Normal-order evaluate will first expand (test 0 (p)) to
-;; (if (= 0 0)
-;;     0
-;;     (p))
-;; Then, the interpreter will substitute (p) with (p) according to the definition of the precedure (p), and substitute (p) with (p) again
-;; since the expression still does not involve only primitive operators, and so on. Hence, the evaluation will get stuck in an infinite
-;; recursion.
+;; Normal-order evaluation will first substitute (p) with (p) according to the definition of (p). Then, it will again substitute (p) with
+;; (p), since (p) is still not a primitive operator, and so on. Hence, the evaluation will get stuck in an infinite recursion.
