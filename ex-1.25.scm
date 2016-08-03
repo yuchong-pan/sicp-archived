d9561c53b6aa3b82c1b1d6dee050fc83b70d6a07
PanYuchong <panyuchong@gmail.com>
2016-08-03 16:59:37 +0800

    Exercise 1.25

---
+++ ex-1.25.scm
@@ -0,0 +1,3 @@
+;; No. Alyssa's procedure has a risk of the value of (fast-expt base exp) being too large. Because arithmetic operations are almost
+;; always performed with limited precision for too small or too large numbers in computers, the value of Alyssa's procedure may not be
+;; precise, and this may engender errors for the fast prime tester.
