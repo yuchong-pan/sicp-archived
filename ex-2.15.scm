9429253b8b4048b120ed8a97c3e2285fb4c7c636
PanYuchong <panyuchong@gmail.com>
2016-08-06 17:13:24 +0800

    Exercise 2.15

---
+++ ex-2.15.scm
@@ -0,0 +1,3 @@
+;; Eva is right.
+;; If some variables that represent an uncertain number are repeated, then the repeated variables will be regarded as different variables,
+;; and hence the percentage tolerance of the resulting interval will be larger than that without repeated variables.
