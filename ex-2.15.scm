3e6e836b959a922e168cab0272683fb8a7cca505
PanYuchong <panyuchong@gmail.com>
2016-08-06 17:26:29 +0800

    Refine Exercise 2.15

--- ex-2.15.scm
+++ ex-2.15.scm
@@ -1,3 +1,8 @@
 ;; Eva is right.
+;;
 ;; If some variables that represent an uncertain number are repeated, then the repeated variables will be regarded as different variables,
 ;; and hence the percentage tolerance of the resulting interval will be larger than that without repeated variables.
+;;
+;; For example, if A is some measurement representing an uncertain number, then A/A should a certain number 1, whereas the system will
+;; regard the two A's as two different measurements with the same interval and give an interval centered at 1 with some percentage
+;; tolerance.
