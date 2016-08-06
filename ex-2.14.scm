72b3271e8a052c1481a201e7a5231db60b6d2fe9
PanYuchong <panyuchong@gmail.com>
2016-08-06 17:08:01 +0800

    Exercise 2.14

---
+++ ex-2.14.scm
@@ -0,0 +1,11 @@
+;; Suppose R1 is a resistor with the center of 100 ohms and the percentage tolerance of 1%, and R2 is a resistor with the center of 200
+;; ohms and the percentage tolerance of 1%.
+;; The parallel-resistor given by par1 is a resister with the center of 83.23 ohms and the percentage tolerance of 21.36%, and that given
+;; by par2 is a resistor with the center of 66.67 ohms and the percentage tolerance of 0.84%.
+;; Hence, Lem is right.
+;;
+;; Suppose A is an interval with the center of 100 and the percentage tolerance of 1%, and B is an interval with the center of 200 and the
+;; percentage tolerance of 1%.
+;; We have A/A is an interval with the center of 1.0002 and the percentage tolerance of 2.00%, and A/B is an interval with the center of
+;; 0.50004 and the percentage tolerance of 2.01%.
+;; Hence, I conclude that, after arithmetic operations, the percentage tolerance will be considerably incremented.
