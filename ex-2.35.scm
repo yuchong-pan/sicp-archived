09d1cbc31a6fb6ddc95816baddc60b505e851438
PanYuchong <panyuchong@gmail.com>
2016-08-08 01:21:44 +0800

    Exercise 2.35

---
+++ ex-2.35.scm
@@ -0,0 +1,12 @@
+(define (count-leaves t)
+  (accumulate + 0 (map (lambda (x) 1) (enumerate-tree t))))
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+(define (enumerate-tree t)
+  (cond ((null? t) nil)
+        ((not (pair? t)) (list t))
+        (else (append (enumerate-tree (car t))
+                      (enumerate-tree (cdr t))))))
