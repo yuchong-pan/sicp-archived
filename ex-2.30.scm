67b831fe7d35e9e19a7246b04bba80cbd56f94f5
PanYuchong <panyuchong@gmail.com>
2016-08-07 20:01:44 +0800

    Exercise 2.30

---
+++ ex-2.30.scm
@@ -0,0 +1,6 @@
+(define (square-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (square tree))
+        (else (cons (square-tree (car tree)) (square-tree (cdr tree))))))
+(define (square x)
+  (* x x))
