6f3de5ae64ef48a0bd9605cc4dc0af24c2d8a53c
PanYuchong <panyuchong@gmail.com>
2016-08-07 20:04:28 +0800

    Exercise 2.31

---
+++ ex-2.31.scm
@@ -0,0 +1,5 @@
+(define (tree-map proc tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (proc tree))
+        (else (cons (tree-map proc (car tree))
+                    (tree-map proc (cdr tree))))))
