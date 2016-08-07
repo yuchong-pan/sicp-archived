537dd32153c7c382d548a8b85a9011714fa85c5c
PanYuchong <panyuchong@gmail.com>
2016-08-07 19:11:01 +0800

    Exercise 2.27

---
+++ ex-2.27.scm
@@ -0,0 +1,6 @@
+(define (deep-reverse items)
+  (define (iter items result)
+    (cond ((null? items) result)
+          ((not (pair? (car items))) (iter (cdr items) (cons (car items) result)))
+          (else (iter (cdr items) (cons (deep-reverse (car items)) result)))))
+  (iter items nil))
