ba34c7e3568590d93aae601c44216702696e8f00
PanYuchong <panyuchong@gmail.com>
2016-08-19 00:08:25 +0800

    Exercise 2.63

---
+++ chapter02/ex-2.63.scm
@@ -0,0 +1,5 @@
+;; a. The 2 procedures produce the same result for every tree; both of them produce the inorder traverse of the given tree. The 2
+;;    procedures both produce '(1 3 5 7 9 11) for each of the trees in Figure 2.16.
+;;
+;; b. The 2 procedures have the same order of growth in the number of steps required to convert a balanced tree with n elements to a list;
+;;    both of them require Theta(n) steps.
