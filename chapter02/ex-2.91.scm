40827d43779d41dfa21eceb78ff2463c5d764102
PanYuchong <panyuchong@gmail.com>
2016-08-25 17:01:36 +0800

    Exercise 2.91

---
+++ chapter02/ex-2.91.scm
@@ -0,0 +1,27 @@
+(define (div-poly p1 p2)
+  (if (same-variable? (variable p1)
+                      (variable p2))
+      (let ((terms (div-terms (term-list p1)
+                              (term-list p2))))
+        (list (make-poly (variable p1)
+                         (car terms))
+              (make-poly (variable p2)
+                         (cadr terms))))
+      (error "Polys not in same var -- DIV-POLY" (list p1 p2))))
+
+(define (div-terms L1 L2)
+  (if (empty-termlist? L1)
+      (list (the-empty-termlist) (the-empty-termlist))
+      (let ((t1 (first-term L1))
+            (t2 (first-term L2)))
+        (if (> (order t2) (order t1))
+            (list (the-empty-termlist) L1)
+            (let ((new-c (div (coeff t1) (coeff t2)))
+                  (new-o (- (order t1) (order t2))))
+              (let ((rest-of-result
+                     (div-terms (sub-terms L1
+                                           (mul-terms L2
+                                                      (adjoin-term (make-term new-o new-c)
+                                                                   (the-empty-termlist)))))))
+                (adjoin-term (make-term new-o new-c)
+                             rest-of-result)))))))
