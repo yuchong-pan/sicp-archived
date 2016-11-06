ab11bbe5c5468af7a5976f2cc2dd2f371f774337
yuchong-pan <panyuchong@gmail.com>
2016-11-06 22:07:39 +0800

    Exercise 3.29

---
+++ chapter03/ex-3.29.scm
@@ -0,0 +1,8 @@
+(define (or-gate a1 a2 output)
+  (let ((b (make-wire))
+        (c (make-wire))
+        (d (make-wire)))
+    (inverter a1 b)
+    (inverter a2 c)
+    (and-gate b c d)
+    (inverter d output)))
