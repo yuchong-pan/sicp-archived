a2cd77aceff39f9546e3b8175d60bd2f3f2cc60c
yuchong-pan <panyuchong@gmail.com>
2018-02-25 01:09:30 -0800

    Exercise 3.41

---
+++ chapter03/ex-3.41.scm
@@ -0,0 +1,4 @@
+;; I do not agree. Allowing unserialized access to the bank balance does not
+;; result in anomalous behavior, because there are no side effects when
+;; accessing the bank balance. There is no scenario that demonstrates Ben's
+;; concern.
