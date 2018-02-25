4e0c162683d2487e1e92bba986195aeb50f2ad53
yuchong-pan <panyuchong@gmail.com>
2018-02-25 03:38:29 -0800

    Exercise 3.44

---
+++ chapter03/ex-3.44.scm
@@ -0,0 +1,5 @@
+;; Louis is not right. The essential difference between the transfer problem
+;; and the exchange problem is that the difference of the two account balances
+;; involved in an exchange may be changed by other concurrent transactions if
+;; the exchange procedure is not serialized, while the transfer amount is given
+;; and will not change in the transfer problem.
